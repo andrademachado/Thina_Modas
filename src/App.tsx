ac9f4bb215e85fd3b5e3a53d95ce6a9537398d6a
Lígia Andrade <andradeligiamp@gmail.com>
2025-09-23 12:14:18 -0300

    Style Global ,definição de cores

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalCss } from "./styles"
+
 function App() {
 
   return (
-    <>
+        <>
+        <GlobalCss/>
       <div>
         <h1>Thina Modas</h1>
       </div>
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalCss = createGlobalStyle`
-    
+const cores = {
+    branco: '#fff',
+    preto: '#0000',
+    pretoCinza: '#000000A8',
+    cinza: '#00000075',
+    rosa: '#fbdeea',
+    rosaBranco: '#F9F5F4',
+    marrom: '#be9061',
+    verde: '#027C3F',
+    vinho: '#79191D'
+}
+
+export const GlobalCss = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family:  Roboto flex, sans-serif;
+    }
+
+    body {
+        background-color: ${cores.branco};
+        color: ${cores.pretoCinza} ;
+    }
 `
+
+
